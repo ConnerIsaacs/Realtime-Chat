9a55e7ccd2811743df7032b28d0a2a5f341b6762
Conner Isaacs <cjisaacs@att.net>
2017-07-05 18:51:49 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
 //initialize dependencies
 const express = require('express');
 const socket = require('socket.io');
-
+const mongoose = require("mongoose");
+//require schema
+const newMessage = require("./index");
 //create app
 const app = express();
 const server = app.listen(8000);
 const io = socket(server);
 
+//connect to mongoose database
+mongoose.connect(process.env.MONGODB_URI="mongodb://Cylthus:079815e23h54tp@ds149412.mlab.com:49412/chat");
 var connects = 0; //number of connected people
 app.use(express.static('public'));
 
 io.on('connection',(socket)=>{
  
     socket.on("chat",(data)=>{
+      var x = new newMessage({
+        Handle: data.handle,
+        Message: data.message,
+      });
+      x.save();
         io.sockets.emit("chat",data);
+      
     })
     socket.on("typing",(data)=>{
         socket.broadcast.emit("typing",data);
@@ -21,6 +31,7 @@ io.on('connection',(socket)=>{
   socket.on("notTyping",(data)=>{
     socket.broadcast.emit("notTyping");
   })
+  
   socket.on("newConnection",()=>{
     connects++;
     io.sockets.emit("newConnection",connects);
@@ -28,5 +39,8 @@ io.on('connection',(socket)=>{
   socket.on("disconnect",()=>{
     connects--;
     io.sockets.emit("loseConnection",connects);
+  })
+  socket.on("announceConnect",()=>{
+    socket.broadcast.emit("announceConnect");
   })
 });
